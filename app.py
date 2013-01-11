a6f359ec63a882cdbb0b539644ac5090b0924350
Vincent Ahrend <nichte@gmail.com>
2013-01-12 00:50:41 +0530

    Revert to SQLAlchemy/SQLite

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ import datetime
 from flask import abort, Flask, request, flash, g, redirect, render_template, url_for, session
 from flask.ext.wtf import Form, TextField as WTFTextField, Required, Email
 from flask.ext.couchdb import CouchDBManager, DateTimeField, Document, TextField, ViewDefinition, ViewField
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import OperationalError
 #from gevent.wsgi import WSGIServer
 from humanize import naturaltime
 from werkzeug.local import LocalProxy
 from werkzeug.contrib.cache import SimpleCache
 
 """ Config """
+DATABASE = '/tmp/khemia.db'
 DEBUG = True
 SEND_FILE_MAX_AGE_DEFAULT = 1
 # TODO: Generate after installation, keep secret.
@@ -17,72 +20,42 @@ SECRET_KEY = '\xae\xac\xde\nIH\xe4\xed\xf0\xc1\xb9\xec\x08\xf6uT\xbb\xb6\x8f\x1f
 PASSWORD_HASH = '8302a8fbf9f9a6f590d6d435e397044ae4c8fa22fdd82dc023bcc37d63c8018c'
 SERVER_NAME = 'app.soma:5000'
 
-COUCHDB_SERVER = 'http://app.soma:5984'
-COUCHDB_DATABASE = 'ark'
-
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.jinja_env.filters['naturaltime'] = naturaltime
 
 
-# Setup CouchDB
-manager = CouchDBManager()
-
 # Setup SQLAlchemy
-#app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
-#db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + DATABASE
+db = SQLAlchemy(app)
 
 
 # Setup Document Types
-class Persona(Document):
-    doc_type = "persona"
+class Persona(db.Model):
+    id = db.Column(db.String(32), primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    private = db.Column(db.Text)
+    public = db.Column(db.Text)
+    starmap = db.relationship('Star', backref=db.backref('creator'))
 
-    username = TextField()
-    email = TextField()
-    private = TextField()
-    public = TextField()
-    created = DateTimeField(default=datetime.datetime.now)
-
-
-class Star(Document):
-    doc_type = 'star'
-
-    text = TextField()
-    creator_id = TextField()
-    creator_name = TextField()
-    created = DateTimeField(default=datetime.datetime.now)
-
-    by_date = ViewField('soma', '''\
-    function (doc) {
-        if (doc.doc_type == 'star') {
-            emit(doc.created, doc);
-        }
-    }''')
-
-    by_creator = ViewField('soma', '''\
-    function (doc) {
-        if (doc.doc_type == 'star') {
-            emit(doc.creator_id, doc);
-        }
-    }
-    ''')
-manager.add_viewdef([Star.by_date, Star.by_creator])
+    def __init__(self, id, username, email, private, public):
+        self.id = id
+        self.username = username
+        self.email = email
+        self.private = private
+        self.public = public
 
 
-# Setup View Definitions
-controlled_personas_view = ViewDefinition('soma', 'controlled_personas', '''\
-        function (doc) {
-            if (doc.doc_type == 'persona' && doc.private != "") {
-                emit(doc.username, doc);
-            }
-        }
-    ''')
-manager.add_viewdef(controlled_personas_view)
+class Star(db.Model):
+    id = db.Column(db.String(32), primary_key=True)
+    text = db.Column(db.Text)
+    creator_id = db.Column(db.String(32), db.ForeignKey('persona.id'))
 
-manager.setup(app)
-
-# Allows access of 'g.couch' through 'couch'
-couch = LocalProxy(lambda: g.couch)
+    def __init__(self, id, text, creator):
+        self.id = id
+        self.text = text
+        self.creator_id = creator
 
 # Setup Cache
 cache = SimpleCache()
@@ -91,16 +64,29 @@ cache = SimpleCache()
 """ DB code """
 
 
+def init_db():
+    try:
+        Persona.query.first()
+    except OperationalError:
+        app.logger.info("Initializing DB")
+        db.create_all()
+        pv = Persona('247a1ca474b04a248c751d0eebf9738f', 'cievent', 'nichte@gmail.com',
+'aAOHDFWvQb4IMQUghVYizG3qbYCtQVsdcpFy5xqjdpBsF7Jjmu3I/Ax5BEyIscfeDJu/lJ1sQSIuiclW4V1KLwfK54LU+dYrD79zLyzU0xoS9tzIo56YUX62CPwd4m3HBbJhmYWo5XTEfh6rfdFdH21PCyZhjF2GZhr+s4ZKgGMju2v3hU8t9IaUaMNLUKEULh0PJFUM88/6Pfl2D1m7hFZCKkfGoKIifiOdTRrvQfkgayIbQ3w7Oixx5z6U8ZWfBKuArY7WSvb4SoKHtDMT0E9iUF2jmkwXhqQqnoTSiuSsgc5AHQlF9fuYw1DuCAkERlDBtgQu6IginiBGLBa3oBahsOElsoEcq3d3p8pAO2CW4SxNex/ZHZeJ8+uO1+CrxQ1f9dcMl5wCzCJEQ8u3VIHW3jMa2prp29OlATPIbPmF6q/cYeDfZibGEeZxhe2gSXHyqyXoqciuWUn9CeIrpwpoRJtyXK5NLgWljMyf6IkgHDPQluU4HAbvxt7Kv8dAJ/ZfAF7HmUb1sKxMemRrc01LwNze/4zYosuq5Ka/GQ3aWjWvOlsE8KNSLjf4X1lzP/enM2l/ilYOueKmH/Bl13U3kQBM7+SPyJjlxylJ0VbyY7BMvwAkJEFCSCRGVho0KSEujKoop3DAdlhzQnUuCmMvW1usHxTuM3y9w+l2HgqgXu7HonHGowMbrUJkuBeZBf48kNm+o5p09Jp/CT0gPawsdLsMCwuegqDrzOxCcCwgK6zYXFf7k0E59qC5/ZtaQWsU9U3u97uHpkTmfmMCn9nWXlDz8+ElHMPiVWeAvvW+VTf8IdbaxqS3IafGMLyCsgbqJKz98DMRjdeXizMXx6zCaTMzHi/PrniZFzEY/Go1t7PtckkgJ4/LofJf8kJMePUiY6FdKX0Cd3TB95Y1/Vx3rjTcZhfquSsqPQMv6lZLg1d6cMu9IWgakIhB59/4YH3UEYLr49f6I2R/1ft8ExE0VS/tqeMXEKL8TRZ7bomw5lLVtRG/ybINaBNIGAPRixT4w08pZgQSV1M1la9Z3YgM4KuhJwDsOV9BPNOEcT8wSQZ+0hKfUOCk4DcivRoA02INj+Po8lRRa6dxTcQX00+4FnTyW5tSZg3p768jsin98HiWl6uP0zw1BKvOleqfV/6QvFfuZeVY1T4NFCx8yX8rCRettqLArL2F97dfOVgeX79hFqXPXyU1LjFkhu6adWJiPPFsKh+9Tg6H9YcDRcU5ZJCTZuamLGbOCCTd+9Gy8/nGyzOjth6jVaOs/lRZT3uZMCfCsdi6dIB08+WfuSQIFQSII9DxqBqwotxX0+Hjmfj5yjH5AK+hOWT7dSFGi0GikufCCW6Z/V5MvoSadjkSeAc6heceqbUhZCOLlcVvBXrulJtrAQctpC028sAkJzur0jaiGmDMMs9tfT4hMtIiyVjoXM2hMkGMyNUu5l77A/sHcvM4b7MIaUiBVpAvs9FLbukKpj70MLc69G617rp53FY22YEk2OPaYB2Vh6D8ivO8e7HaDlUm3dFis84tJBQM5tT5IXou9wRViYc0kDW07g0e3GFlpy9VIizdUPf72CnGiZ0spSVOPtD88uYp+N2lpThalXvrIDD17c1s4GCNAI68KH5RtCAt0YpZ3j/i5zTN25KGEf9i/MVhtWSgfDgHEAw+ni+p6aKXZEcKIH8YrDY/Q9qsdZxs6CR0AeVqo3rk1YKQSA3CMmkaq8w9ITl1F+9nHCpnj9j8G2wKA7RAGkrREnl4HsLcMB6Ul7LoH8Pa9qBQMDdeCYrf7VpZmRY8099bZyj/OtxhJpglsfOD/j2O60TjDSFDZPqezSlA0ksO/xetDSsSDNtgfZKAesrfSVk+XDNd8UhNNgPmMd5mUoaKAl1hqrqJ7sIkXkMM+feVaDPoXqkbUWak3GlptxEPJbHsZLSIeLbOQ3YZBBLyOB9CE1syBI7MziCArxcqUPT8oM/w6DWm/OjiZwFFRcN3LneWGs4EO7TxNBof+spmj2AkBhAzOPhARggs6ME3eSyu6LkMEP7lBgYUODhP6voJqGE/buJm/5EX2iIYIXz5VuvJ/ZTotFleYzl09AFm0wdjv29RdC2m6eMcvn8/t6uJEoo7CRdNaQ9QaGrciRg+3sei5Y5UT7gpsgDbSP7zxcSW+dt0zICRGFrUBPmsKC8LfNSNTXrX1tdg6B4M+2OyfngjPg1vVmMNsBjYXdGjB8fyf2wj8B7djmU0XA3+ZhpYRxn8BMpYcLr/TGwniw==',
+'23qnR6roS0Xp3wSezqzTRuGQm/kS7nOQfwtBHq+tQbVfHhm2MmtXu2As+BzmsZWn6yTO0IiunX68S44nHlNt0w4UYBwVB+2Nfm6+aj7FKGcjU20+vX5BjhLD7NgZv2ALA7UmWVuk/45zBmsmhh8jvQ4klxMbpR3NB+waRbdd5kARhLNMI6sGgy8YTNqwlY5xdNWFd8DB1BdMnMHvUqD28qbSptTzEi+vXSNtoL5bOax1m/MZScWr6Ai6LvMK0M14UmyJXneizmBFD6OV5zGTBf+8RlllS8uPZPjDsrnE9vKgfscQlVrMvP55+uD/h+5KvdPw1+bRKRiEkgeiL2Bo9fqF8P72/i62D8kZQDazkvO/B/q04nhLy1ai9y0S6Ua1fE6K0chIHr5P/OoyOE/tGQZ5qg1SjPLUwiy0NDjQ/VDSrRqDP3WnvbKAgSwX6CZS8aONnxqhatSFevJsUS+hwRR5o8mBP9xRg0DHQiCMKc144CHYXX5jCDx43vFr27JetEKRQ2toajk2n442ZofmVAoTFAygUFJ8zgg7vPpflBxZ6q5c5pWe9Bh4lwUziySgG3O1W0uGaimsnBzeP5jaI3qMj2kjUbQJmYdQm42ZURLrucCdUKrQEJ7ieOTf0oUj')
+        db.session.add(pv)
+        db.session.commit()
+
+
 def get_active_persona():
     """ Return the currently active persona or 0 if there is no controlled persona. """
 
     if 'active persona' not in session or session['active_persona'] is None:
-        controlled_personas = controlled_personas_view()
+        controlled_personas = Persona.query.filter('private != ""')
 
-        if len(controlled_personas) == 0:
-            return False
+        if controlled_personas.first() == None:
+            return ""
         else:
-            session['active_persona'] = controlled_personas.rows[0].value['_id']
+            session['active_persona'] = controlled_personas.first().id
 
     return session['active_persona']
 
@@ -111,7 +97,7 @@ def logged_in():
 
 @app.context_processor
 def persona_context():
-    return dict(controlled_personas=controlled_personas_view().rows)
+    return dict(controlled_personas=Persona.query.filter('private != ""'))
 
 
 @app.before_request
@@ -129,9 +115,9 @@ def before_request():
         app.logger.info("Redirecting to Setup")
         return redirect(url_for('setup', _external=True))
 
-    #if request.base_url not in [setup_url, login_url] and not logged_in():
-    #    app.logger.info("Redirecting to Login")
-    #    return redirect(url_for('login', _external=True))
+    if request.base_url not in [setup_url, login_url] and not logged_in():
+        app.logger.info("Redirecting to Login")
+        return redirect(url_for('login', _external=True))
 
 
 @app.teardown_request
@@ -192,23 +178,9 @@ def setup():
 def persona(id):
     """ Render home view of a persona """
 
-    persona = Persona.load(id)
-    if persona is None:
-        abort(404)
+    persona = Persona.query.filter_by(id=id).first_or_404()
 
-    stars = Star.by_creator()[id].rows[:4]
-
-    starmap = list()
-    for star in stars[:5]:
-        starmap.append({
-            "id": star.id,
-            "created": star.created,
-            "title": star.title if "title" in star else None,
-            "text": [star.text, ],
-            "creator": {
-                "id": star.creator_id,
-                "name": star.creator_name}
-        })
+    starmap = Star.query.filter_by(creator_id=id)[:4]
 
     vizier = Vizier([
         [1, 5, 6, 2],
@@ -279,13 +251,13 @@ def create_persona():
 
         # Save persona to DB
         p = Persona(
-                id=uuid,
-                active=False,
-                username=request.form['name'],
-                email=request.form['email'],
-                private=b64encode(key_private),
-                public=b64encode(key_public))
-        p.store()
+            uuid,
+            request.form['name'],
+            request.form['email'],
+            private=b64encode(key_private),
+            public=b64encode(key_public))
+        db.session.add(p)
+        db.session.commit()
 
         flash("New persona {} created!".format(p.username))
         return redirect(url_for('persona', id=uuid))
@@ -306,9 +278,9 @@ def create_star():
     """ Create a new star """
 
     # TODO: Allow selection of author persona
-    if session['active_persona'] == '0':
+    if session['active_persona'] == "":
         abort(404)
-    creator = Persona.load(session['active_persona'])
+    creator = session['active_persona']
 
     form = Create_star_form()
     if form.validate_on_submit():
@@ -316,11 +288,11 @@ def create_star():
         uuid = uuid4().hex
 
         new_star = Star(
-            id=uuid,
-            text=request.form['text'],
-            creator_id=creator.id,
-            creator_name=creator.username)
-        new_star.store()
+            uuid,
+            request.form['text'],
+            creator)
+        db.session.add(new_star)
+        db.session.commit()
 
         flash('New star created!')
         return redirect(url_for('star', id=uuid))
@@ -330,20 +302,7 @@ def create_star():
 @app.route('/')
 def universe():
     """ Render the landing page """
-
-    stars = Star.by_date().rows
-
-    constellation = list()
-    for star in stars[:4]:
-        constellation.append({
-            "id": star.id,
-            "created": star.created,
-            "title": star.title if "title" in star else None,
-            "text": [star.text, ],
-            "creator": {
-                "id": star.creator_id,
-                "name": star.creator_name}
-        })
+    stars = Star.query.all()
 
     vizier = Vizier([
         [1, 1, 6, 4],
@@ -352,24 +311,14 @@ def universe():
         [7, 1, 2, 5]
     ])
 
-    return render_template('universe.html', layout="sternenhimmel", constellation=constellation, vizier=vizier)
+    return render_template('universe.html', layout="sternenhimmel", constellation=stars, vizier=vizier)
 
 
 @app.route('/s/<id>/', methods=['GET'])
 def star(id):
     """ Display a single star """
-    star = Star.load(id)
-    creator = Persona.load(star.creator_id)
-
-    star = {
-        "id": star.id,
-        "title": star.title if "title" in star else None,
-        "text": [star.text, ] if "text" in star else None,
-        "created": star.created,
-        "creator": {
-            "id": star.creator_id,
-            "name": star.creator_name
-        }}
+    star = Star.query.filter_by(id=id).first_or_404()
+    creator = Creator.query.filter_by(id=id)
 
     return render_template('star.html', layout="star", star=star, creator=creator)
 
@@ -411,6 +360,7 @@ class Vizier():
 
 
 if __name__ == '__main__':
+    init_db()
     # flask development server
     app.run()
 
