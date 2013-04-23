8469dd81fb6a5c53af6a979fd002ce66a648bf03
Vincent Ahrend <vahrend@uos.de>
2013-04-23 02:13:08 +0200

    set_hosts.py now works on windows

--- set_hosts.py
+++ set_hosts.py
@@ -3,8 +3,16 @@
 # This script adds a host entry for app.soma => 127.0.0.1
 # so the app can be accessed at app.soma"""
 
-# TODO: Where is hosts on Windows?
-HOSTSFILE = '/etc/hosts'
+import os
+from sys import platform
+
+if platform == 'win32':
+    WINDIR = os.environ["WINDIR"]
+    HOSTSFILE = os.path.join(WINDIR, 'system32', 'drivers', 'etc', 'hosts')
+else:
+    # Is this really the place on all systems other than Windows?
+    HOSTSFILE = '/etc/hosts'
+
 SOMA_ENTRY = "127.0.0.1 app.soma"
 entry_found = False
 
@@ -15,9 +23,10 @@ def test_host_entry():
         for line in f.readlines():
             if line == SOMA_ENTRY:
                 return True
+    return False
 
 
-if test_host_entry() is False:
+if not test_host_entry():
     print "[soma] adding entry for app.soma to etc/hosts.."
     with open(HOSTSFILE, 'a') as f:
         f.write("\n\n# Access Soma at app.soma\n" + SOMA_ENTRY)
