ee8b6379876917e849d5bbb8ed6b4ffdd24922c0
Vincent Ahrend <nichte@gmail.com>
2013-05-11 20:26:00 +0200

    SERVER_PORT needs to be int

--- testing_config.py
+++ testing_config.py
@@ -9,7 +9,7 @@ LOG_FORMAT = (
 
 # Define addresses
 SERVER_HOST = 'glia.herokuapp.com'
-SERVER_PORT = os.environ['PORT']
+SERVER_PORT = int(os.environ['PORT'])
 SERVER_NAME = "{}:{}".format(SERVER_HOST, SERVER_PORT)
 
 # Define database setup
