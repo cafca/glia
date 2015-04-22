946f47866b883500f06bbe97015b56fe7bd54c55
Vincent Ahrend <nichte@gmail.com>
2015-04-22 14:06:40 +0200

    Load HTTP credentials from env vars

--- glia/web/dev_helpers.py
+++ glia/web/dev_helpers.py
@@ -8,6 +8,7 @@
     :copyright: (c) 2015 by Vincent Ahrend.
 """
 import logging
+import os
 
 from flask.ext.httpauth import HTTPDigestAuth
 
@@ -16,15 +17,24 @@ from flask.ext.httpauth import HTTPDigestAuth
 #
 
 http_auth = HTTPDigestAuth()
-http_auth_users = {
-    "admin_user": "coaltarp1010",
-    "testing_user": "PVi&OSpRNqRSghJ62W0@dcMAdYl#MVBx"
-}
+
+if os.environ['RK_HTTPAUTH_ADMIN'] is None:
+    # Fallback for local dev environment
+    http_auth_users = {
+        "admin_user": "coaltarp1010",  # mm so nice to type
+        "testing_user": "PVi&OSpRNqRSghJ62W0@dcMAdYl#MVBx"
+    }
+else:
+    http_auth_users = {
+        "admin_user": os.environ['RK_HTTPAUTH_ADMIN'],
+        "testing_user": os.environ['RK_HTTPAUTH_USER']
+    }
 
 
 @http_auth.get_password
 def get_pw(username):
     if username in http_auth_users:
+        logging.info("HTTPAuth: {} authenticated".format(username))
         return http_auth_users.get(username)
     logging.warning("Attempted login by user not in HTTP auth user list")
     return None
