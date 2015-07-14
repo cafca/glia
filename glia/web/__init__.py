df604b352b336824c383c47f7eb12db9068f2307
Vincent Ahrend <nichte@gmail.com>
2015-07-14 12:37:38 +0200

    Cache Persona movements

--- glia/web/__init__.py
+++ glia/web/__init__.py
@@ -38,12 +38,7 @@ def inject_navbar_movements():
     if current_user.is_anonymous():
         movements = Movement.top_movements()
     else:
-        user_movements = Movement.query \
-            .join(Mma) \
-            .filter_by(persona=current_user.active_persona)
-
-        movements = [dict(id=m.id, username=m.username)
-            for m in user_movements]
+        movements = current_user.active_persona.movements()
 
     return dict(nav_movements=movements)
 
