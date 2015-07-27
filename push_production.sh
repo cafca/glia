680e2cc98cc22e3a7fab3c58fee44637cfdcfbc2
Vincent Ahrend <nichte@gmail.com>
2015-07-27 22:04:39 +0200

    Fix deployment script

--- push_production.sh
+++ push_production.sh
@@ -3,4 +3,4 @@ git checkout master &&
 git merge dev &&
 git push heroku master &&
 heroku run python manage.py db upgrade --app=glia &&
-git checkout dev &&
+git checkout dev
--- push_testing.sh
+++ push_testing.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git push heroku-dev dev:master &&
-heroku run python manage.py db upgrade --app=glia-dev &&
+heroku run python manage.py db upgrade --app=glia-dev
